eee6139ce00173225357847100988b0365763aad
Anima <18208134+animafps@users.noreply.github.com>
2021-12-10 14:46:13 +1300

    better parsing of the body

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
     const url = new URL(request.url)
-    if (request.method !== 'POST') {
+    if (request.method !== 'POST')
         return new Response('Not found', { status: 404 })
-    }
     const requestBody = await request.json()
-    if (!requestBody.release) return new Response('Error', { status: 500 })
+    if (requestBody.action !== 'published')
+        return new Response('Error', { status: 500 })
 
     await fetch(DISCORD_URL, {
         method: 'POST',
@@ -19,11 +19,19 @@ async function handleRequest(request) {
                     title: requestBody.release.tag_name,
                     url: requestBody.release.html_url,
                     timestamp: requestBody.release.published_at,
-                    description: requestBody.release.body,
+                    fields: parseBody(requestBody.release.body),
                 },
             ],
         }),
     })
 
     return new Response('OK')
+}
+
+function parseBody(body) {
+    const splitHeaders = body.split('### ').shift()
+    return splitHeaders.flatMap(val => {
+        const split = val.split('\n\n')
+        return { name: split[0], value: split[1] }
+    })
 }
